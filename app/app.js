689501dbebc99ec289545c6c672416a44281c67a
Aishi Jiang <jiangaishi@gmail.com>
2015-04-06 17:03:01 -0400

    before messing around with the shared data between search query

--- app/app.js
+++ app/app.js
@@ -1,6 +1,10 @@
 
 var routerApp = angular.module('routerApp', ['ui.router']);
 
+routerApp.factory('SearchData', function(){
+    return "I am data from a service"
+});
+
 routerApp.config(function($stateProvider, $urlRouterProvider) {
     
     $urlRouterProvider.otherwise('/trending');
@@ -13,7 +17,7 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
                 '': { templateUrl: 'app/modules/partial-home.html' },
                 'navbar@trending': { 
                     templateUrl: 'app/modules/navbar.html',
-                    controller: 'issueCtrl'
+                    controller: 'navBarCtrl'
                 },
                 'sidebar@trending': { templateUrl: 'app/modules/sidebar-signup.html' },
                 'mainview@trending': { 
@@ -29,7 +33,7 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
                 '': { templateUrl: 'app/modules/partial-home.html' },
                 'navbar@london': { 
                     templateUrl: 'app/modules/navbar.html',
-                    controller: 'issueCtrl'
+                    controller: 'navBarCtrl'
                 },
                 'sidebar@london': { templateUrl: 'app/modules/sidebar-signup.html' },
                 'mainview@london': { 
@@ -48,7 +52,7 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
                 '': { templateUrl: 'app/modules/partial-signup.html' },
                 'navbar@signup': { 
                     templateUrl: 'app/modules/navbar.html',
-                    controller: 'issueCtrl'
+                    controller: 'navBarCtrl'
                 },
                 'fullview@signup': { 
                     templateUrl: 'app/modules/signup.html',
@@ -62,7 +66,7 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
                 '': { templateUrl: 'app/modules/partial-home.html' },
                 'navbar@bridgewater': { 
                     templateUrl: 'app/modules/navbar.html',
-                    controller: 'issueCtrl'
+                    controller: 'navBarCtrl'
                 },
                 'sidebar@bridgewater': { templateUrl: 'app/modules/sidebar-signup.html' },
                 'mainview@bridgewater': { 
@@ -77,7 +81,9 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
 });
 
 
-routerApp.controller('issueCtrl', function($scope, $http) {
+routerApp.controller('issueCtrl', function($scope, $http, SearchData) {
+    $scope.query = SearchData;
+
     $http.get('issue.js')
     .success(function(response) 
         {
@@ -85,3 +91,91 @@ routerApp.controller('issueCtrl', function($scope, $http) {
         });
 
 });
+
+routerApp.controller('navBarCtrl', function($scope, $http, SearchData) {
+    $scope.query = SearchData;
+
+});
+
+
+
+
+
+
+
+
+
+
+
+routerApp.constant('AUTH_EVENTS', {
+  loginSuccess: 'auth-login-success',
+  loginFailed: 'auth-login-failed',
+  logoutSuccess: 'auth-logout-success',
+  sessionTimeout: 'auth-session-timeout',
+  notAuthenticated: 'auth-not-authenticated',
+  notAuthorized: 'auth-not-authorized'
+});
+
+routerApp.constant('USER_ROLES', {
+  all: '*',
+  admin: 'admin',
+  editor: 'editor',
+  guest: 'guest'
+});
+
+routerApp.controller('LoginController', function ($scope, $rootScope, AUTH_EVENTS, AuthService) {
+  $scope.credentials = {
+    username: '',
+    password: ''
+  };
+  $scope.login = function (credentials) {
+    AuthService.login(credentials).then(function (user) {
+      $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+      $scope.setCurrentUser(user);
+    }, function () {
+      $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+    });
+  };
+})
+
+routerApp.factory('AuthService', function ($http, Session) {
+  var authService = {};
+ 
+  authService.login = function (credentials) {
+    return $http
+      .post('/login', credentials)
+      .then(function (res) {
+        Session.create(res.data.id, res.data.user.id,
+                       res.data.user.role);
+        return res.data.user;
+      });
+  };
+ 
+  authService.isAuthenticated = function () {
+    return !!Session.userId;
+  };
+ 
+  authService.isAuthorized = function (authorizedRoles) {
+    if (!angular.isArray(authorizedRoles)) {
+      authorizedRoles = [authorizedRoles];
+    }
+    return (authService.isAuthenticated() &&
+      authorizedRoles.indexOf(Session.userRole) !== -1);
+  };
+ 
+  return authService;
+});
+
+routerApp.service('Session', function () {
+  this.create = function (sessionId, userId, userRole) {
+    this.id = sessionId;
+    this.userId = userId;
+    this.userRole = userRole;
+  };
+  this.destroy = function () {
+    this.id = null;
+    this.userId = null;
+    this.userRole = null;
+  };
+});
+
